1e0bfed2fb50b8f54166f40c2a383dd47388b6de
777024 <tonyprince335@gmail.com>
2022-10-01 21:02:51 +0800

    add cooking float
    +oil pan
    +cut & add ingredients
    +seasoning

--- Assets/Resources/Script/Manager.cs
+++ Assets/Resources/Script/Manager.cs
@@ -16,8 +16,11 @@ public class Manager : MonoBehaviour
     Camera cameraM;
     RaycastHit2D hit;
     Pan panScript;
+    Ingredients ingredientsScript;
+    public GameObject ingredientOnBoard;
 
-    private void Awake() {
+    private void Awake()
+    {
         sink = GameObject.Find("Sink");
         cutBoard = GameObject.Find("CutBoard");
         pan = GameObject.Find("Pan");
@@ -26,23 +29,58 @@ public class Manager : MonoBehaviour
         plate = GameObject.Find("plate");
         ingredients = GameObject.Find("Ingredients");
         cameraM = Camera.main;
-        
+
     }
 
-    private void Start() {
+    private void Start()
+    {
         panScript = pan.GetComponent<Pan>();
+        ingredientsScript = ingredients.GetComponent<Ingredients>();
     }
 
-    private void Update() {
+    private void Update()
+    {
         hit = Physics2D.Raycast(cameraM.ScreenToWorldPoint(MousePos()), Vector2.zero);
         if (hit.collider != null)
         {
             // Debug.Log(hit.collider.gameObject);
-            AddOil(hit.collider.gameObject);
+            AddIngredients(hit.collider.gameObject);
+            DragIngredient();
+            if (hit.collider.gameObject.name == "CutBoard" && ingredientOnBoard != null && panScript.ingredients == null)
+            {
+                panScript.SetIngredients(ingredientOnBoard);
+                ingredientOnBoard = null;
+            }
+        }
+        isGameOver();
+    }
+
+    void DragIngredient()
+    {
+
+        if (hit.collider.gameObject.name == "Ingredients")
+        {
+            if (ingredientOnBoard == null)
+            {
+                if (Random.value < 0.3f)
+                {
+                    ingredientOnBoard = ResourcesPool.GetInstance().GetObj("Potato");
+                }
+                else if (Random.value < 0.6f)
+                {
+                    ingredientOnBoard = ResourcesPool.GetInstance().GetObj("Broccoli");
+                }
+                else if (Random.value < 1f)
+                {
+                    ingredientOnBoard = ResourcesPool.GetInstance().GetObj("Tomato");
+                }
+                ingredientOnBoard.transform.position = new Vector3(-5.07f,1.88f,0);
+            }
         }
     }
 
-    Vector3 MousePos() {
+    Vector3 MousePos()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             return Input.mousePosition;
@@ -50,14 +88,20 @@ public class Manager : MonoBehaviour
         return Vector3.zero;
     }
 
-    void AddOil(GameObject gameObject){
+    void AddIngredients(GameObject gameObject)
+    {
         if (gameObject.name == "Oil")
         {
             panScript.Setoil(gameObject);
         }
+        if (gameObject.name == "Seasonings")
+        {
+            panScript.SetSeasoning(gameObject);
+        }
     }
 
-    void isGameOver(){
+    void isGameOver()
+    {
         if (panScript.burnOff)
         {
             Debug.Log("GAME OVER");
--- Assets/Resources/Script/Pan.cs
+++ Assets/Resources/Script/Pan.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Pan : MonoBehaviour
 {
     public GameObject oil;
+    public GameObject ingredients;
+    public GameObject seasonings;
     public bool burnOff = false;
     public bool onFire = false; 
+    public bool recipieDone = false;
     public int putOffCount = 10;
     Collider2D colliderPan;
  
-    // if pan has ingredients -> cook recipies in 3 seconds
+    // if pan has ingredients -> recipies burn after 10s with your pan
     // if pan only has oil    -> set on fire in 3 seconds
     // if pan was null        -> burn off the pan in 10 sec, game over
     void Start()
@@ -24,6 +27,19 @@ public class Pan : MonoBehaviour
     {
         if (oil != null)
         {
+            if (ingredients != null && seasonings != null)
+            {
+                StartCoroutine("RecipieBurn");
+                colliderPan.enabled = true;
+                if (recipieDone)
+                {
+                    StopCoroutine("RecipieBurn");
+                    Debug.Log("recipie burn stop");
+                    recipieDone = false;
+                    CleanPan();
+                }
+            }
+            else
             StartCoroutine("OilOnFire");
         }
         PutOutFire();
@@ -33,35 +49,69 @@ public class Pan : MonoBehaviour
         oil = gameObject;
     }
 
+    public void SetIngredients(GameObject gameObject){
+        ingredients = gameObject;
+    }
+
+    public void SetSeasoning(GameObject gameObject){
+        seasonings = gameObject;
+    }
+    void CleanPan(){
+        oil = null;
+        ingredients = null;
+        seasonings = null;
+    }
+
     void PutOutFire(){
-        if (onFire == true)
+        if (onFire)
         {
             colliderPan.enabled = true;
+            StartCoroutine("Timer");
             StopCoroutine("OilOnFire");
             if (putOffCount == 0)
             {
+                StopCoroutine("Timer");
                 onFire = false;
                 Debug.Log("put off");
                 putOffCount = 10;
                 colliderPan.enabled = false;
+                CleanPan();
             }
+        }
+        if (burnOff)
+        {
+            StopCoroutine("Timer");
         }
     }
 
     private void OnMouseDown() {
-        putOffCount -= 1;
+        if (ingredients != null){
+            recipieDone = true;
+        }else{
+            putOffCount -= 1;
+        }
     }
     void BurnOff(){
         burnOff = true;
+        Debug.Log("Burn off");
     }
 
     private IEnumerator OilOnFire(){
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(4f);
         Debug.Log("onfire");
         onFire = true;
     }
 
     private IEnumerator Timer(){
-        yield return new WaitForSeconds(1f);
+        Debug.Log("Timer");
+        yield return new WaitForSeconds(6f);
+        BurnOff();
     }
+
+    private IEnumerator RecipieBurn(){
+        Debug.Log("RecipieBurn");
+        yield return new WaitForSeconds(10f);
+        BurnOff();
+    }
+    
 }
